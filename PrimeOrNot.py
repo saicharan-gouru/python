8a0ba096b4ed119ea5952cf22461488d0d8c1785
saicharan <62795476+saicharan-gouru@users.noreply.github.com>
2021-04-30 21:10:55 +0530

    Update PrimeOrNot.py

--- PrimeOrNot.py
+++ PrimeOrNot.py
@@ -1,7 +1,10 @@
 a=int(input("Enter a number:"))
-
-for i in range(2,a-1):
-    if a%i==0:
-        print(a,"is not prime")
-else:        
-    print(a,"is prime")
+if a>1:
+    for i in range(2,a-1):
+        if a%i==0:
+            print(a,"is not prime") 
+            break
+    else:       
+        print(a,"is prime")
+else:
+    print(a,"is not prime")
