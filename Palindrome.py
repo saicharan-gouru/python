4c33bc7b39ca457c88cbc49011650df4ad5718c3
saicharan <62795476+saicharan-gouru@users.noreply.github.com>
2021-05-04 20:14:45 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -2,10 +2,13 @@
 A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward, such as madam or 
 racecar.
 '''
-A=input('Enter a word, number, phrase, or other sequence of characters:')
-print('Given input:',A)
-S=A[::-1]
-if S==A:
-    print('Given Input Is Palindrome :)')
-else:
-    print('Given Input Is Not Palindrome :( , Try Another One')
+Flag=0
+while(Flag==0):
+    A=input('Enter a word, number, phrase, or other sequence of characters:')
+    print('Given input:',A)
+    S=A[::-1]
+    if S==A:
+        print('Given Input Is Palindrome :)')
+    else:
+        print('Given Input Is Not Palindrome :( , Try Another One')
+    Flag=int(input('Enter 0 to continue:'))
