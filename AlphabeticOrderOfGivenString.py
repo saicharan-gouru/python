79898c68a8706c0900dafbf0462821c9b6b7c6fa
Saicharan Gouru <gourusaicharan6@gmail.com>
2021-06-23 14:24:31 +0530

    Create AlphabeticOrderOfGivenString.py

---
+++ AlphabeticOrderOfGivenString.py
@@ -0,0 +1,10 @@
+def alp(s):
+  return ''.join(sorted(s))
+
+userInput=input()
+print("Before arranging in alphabetic order : ",userInput)
+print("After arranging in alphabetic order : ",alp(userInput))
+
+  
+  
+  
